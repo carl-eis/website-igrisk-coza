f6ad657b85bf4a6a90f76b79040b7aba8207df08
Carl Eiserman <carleiserman@gmail.com>
2017-09-09 14:59:57 +0200

    feature: added data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
+
+import {DataService} from "./services/data.service";
 
 @NgModule({
     declarations: [
@@ -14,9 +19,12 @@ import { LoginComponent } from './pages/login/login.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
     ],
-    providers: [],
+    providers: [DataService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
