a7fce1112ecf7b27d40ecd25f6c017a213d29fe9
Carl Eiserman <carleiserman@gmail.com>
2017-09-10 12:40:11 +0200

    feature: finished services page

--- src/app/components/widgets/services-multi-widget/services-multi-widget.component.ts
+++ src/app/components/widgets/services-multi-widget/services-multi-widget.component.ts
@@ -1,11 +1,15 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Service} from "../../../classes/service";
+import * as Masonry from 'masonry-layout';
+
+declare var $: any;
 
 @Component({
     selector: 'app-services-multi-widget',
     templateUrl: './services-multi-widget.component.html',
     styleUrls: ['./services-multi-widget.component.scss']
 })
+
 export class ServicesMultiWidgetComponent implements OnInit, Input {
 
     @Input() services: Service[];
@@ -15,6 +19,24 @@ export class ServicesMultiWidgetComponent implements OnInit, Input {
     }
 
     ngOnInit() {
+        this.runMasonry();
+    }
+
+    runMasonry(): void {
+        $(document).ready(() => {
+            // let widget_masonry = new Masonry("#services-grid", {
+            //     itemSelector: '.col-md-4'
+            // });
+            $(() => {
+                console.log("Mathing height");
+                $('.service-box').matchHeight();
+            });
+
+
+        });
+    }
+
+    addService(): void {
 
     }
 
